01489cea04b27bbe5e87a7b9f6d61ca5bb829a6a
Andreia Dias <andreiadias2311@gmail.com>
2025-11-05 18:25:55 -0300

    classe pare presentar um cachorro
    Implemente uma classe Dog com atributos e um método late.

---
+++ classe pare presentar um cachorro.py
@@ -0,0 +1,18 @@
+# faça uma classe pare presentar um cachorro
+# Caracteristicas
+# nome
+# cor
+# raca
+
+Class Dog:
+    def __init__(self,nome,cor,raça): #init = iniciar a classe
+        self.nome = nome 
+        self.cor = cor
+        self.raça = raça
+    def latir(self):
+        return "auau"
+#objeto daclasse dog
+dog1 = Dog(nome="Totó",cor="caramelo",raça="indefinida")
+print(dog1)
+print(dog1.latir())
+
