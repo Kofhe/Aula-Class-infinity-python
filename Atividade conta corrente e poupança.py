51957826cfbc414438031ab11d6c88de38a55064
Andreia Dias <andreiadias2311@gmail.com>
2025-11-12 18:39:44 -0300

    Atividade conta corrente e poupança.py

---
+++ Atividade conta corrente e poupança.py
@@ -0,0 +1,34 @@
+#faça uma classe pai chamada Conta
+#nela vai ter as caracteristicas: numero_conta, titular, saldo(não é recebido pelo usuário)
+
+#2classes filhas
+#conta corrente e conta poupança
+
+#saldo não é recebido pelo usuario
+from datetime import datetime
+
+class Conta:
+    def __init__(self,numero_conta,titular):
+        self.numero_conta = numero_conta
+        self.titular = titular
+        self.saldo = 0
+        self.data_criacao=datetime.strftime(datetime.now().date(),"%d/%m/%Y")#marca o momento de criação desta entidade
+
+#classe corrente
+class Corrente(Conta):
+    def __init__(self, numero_conta, titular):
+        super().__init__(numero_conta, titular)
+        self.cheque_especial=100
+        self.credito=400
+
+cliente1 = Corrente(numero_conta="0123",titular="Sei la")
+print(cliente1.saldo)
+
+#classe poupança
+class Poupança(Conta):
+    def __init__(self, numero_conta, titular):
+        super().__init__(numero_conta, titular)
+        self.rendimento=6
+
+cliente2=Poupança(numero_conta="6789",titular="Batata")
+print(cliente2.data_criacao)
