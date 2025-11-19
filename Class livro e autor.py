e994c5808d9721c1001a52f3ac269e3ea257373e
Andreia Dias <andreiadias2311@gmail.com>
2025-11-19 17:49:36 -0300

    Class livro e autor.py

---
+++ Class livro e autor.py
@@ -0,0 +1,18 @@
+class Autor:
+    def __init__(self,nome,data_nascimento):
+        self.nome = nome
+        self.data_nascimento = data_nascimento
+
+class Livro:
+    def __init__(self, titulo, anoPub,autor:Autor):
+        self.titulo = titulo
+        self.anoPub = anoPub
+        self.autor = autor
+
+a1 = Autor(nome="CS Lewis",data_nascimento="13/05/1880")
+l1 = Livro("Como para de ser noob no clash",2025,a1)
+
+print(l1.titulo)
+print(l1.anoPub)
+print(l1.autor.nome)
+print(l1.autor.data_nascimento)
